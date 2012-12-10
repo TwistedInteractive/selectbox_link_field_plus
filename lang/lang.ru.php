226ca50c47252ec0051bcbfdf683db6c97b5b911
alexbirukov <lord_maloy@mail.ru>
2012-12-10 23:05:28 +0600

    Add Russian language
    Add Russian language file

---
+++ lang/lang.ru.php
@@ -0,0 +1,75 @@
+<?php
+
+	$about = array(
+		'name' => 'Русский',
+		'author' => array(
+			'name' => 'Александр Бирюков',
+			'email' => 'info@alexbirukov.ru',
+			'website' => 'http://alexbirukov.ru'
+		),
+		'release-date' => '2012-09-28'
+	);
+
+	/**
+	 * Selectbox Link Field Plus
+	 */
+	$dictionary = array(
+
+		'Select Box Link +' => 
+		'Select Box Link +',
+
+		'<br />%s Only show entries created by the parent entry' => 
+		'<br />%s Показать записи, относящиеся только к родительской записи',
+
+		'View' => 
+		'Вид',
+
+		'<br />%s Enable Create button' => 
+		'<br />%s Разрешить создание записей',
+
+		'<br />%s Enable Edit button' => 
+		'<br />%s Разрешить редактирование',
+
+		'<br />%s Enable Delete button' => 
+		'<br />%s Разрешить удаление',
+
+		'This field will be enabled after you create the entry.' => 
+		'Данное поле станет активным после создания записи.',
+
+		'Autocomplete' => 
+		'Автозавершение',
+
+		'(drag to reorder)' => 
+		'(сортировка перетаскиванием)',
+
+		'Select none' => 
+		'Выберите запись',
+
+		'%s <span class="text">%s</span>' => 
+		'%s <span class="text">%s</span>',
+
+		'Checkboxes' => 
+		'Чекбоксы',
+
+		'Default View' => 
+		'Вид по умолчанию',
+
+		'Gallery (select a field of the type \'upload\')' => 
+		'Галерея (укажите поле с типом \'upload\')',
+
+		'Edit this item' => 
+		'Редактировать запись',
+
+		'Delete this item' => 
+		'Удалить запись',
+
+		'Create new entry in' => 
+		'Создать новую запись в',
+
+		'Go' => 
+		'Перейти',
+
+		'%s hide others' => 
+		'%s скрыть остальные',
+
+	);
