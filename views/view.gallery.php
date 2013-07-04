e94a9eebc9c289729c723e572959e902da1be887
Giel Berkers <info@gielberkers.com>
2013-07-04 09:32:04 +0200

    Update view.gallery.php

--- views/view.gallery.php
+++ views/view.gallery.php
@@ -1,16 +1,5 @@
 <?php
-	/**
-	 * (c) 2011
-	 * Author: Giel Berkers
-	 * Date: 10-10-11
-	 * Time: 14:19
-	 */
-
-
-
 	require_once(EXTENSIONS.'/selectbox_link_field_plus/views/view.php');
-
-
 
 	// The class name must be 'SBLPView_[filename - view. and .php (ucfirst)]':
 	Class SBLPView_Gallery extends SBLPView
